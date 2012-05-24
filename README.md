ec44ff709303770bbd5dda4ee3c8c17615b6121a
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-05-24 04:25:51 +0300

    ilk

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ne?
+
+STI (Single Table Inheritance)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StiDemo::Application.config.secret_token = 'd9844985cd0cc337bc5bef53e175a8332cd95fad59cd402f5ed0c6f104cb1e5c53726aa21daa6729261640a0fadbe3d3a6a4539a9d46241688b0d1fe015c02ab'
